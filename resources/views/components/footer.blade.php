ba130048d1097ba0b69c367973eef587d40fdb26
ptrjs <peterjose64@gmail.com>
2024-05-18 01:02:39 +0700

    dashboard admin

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,9 @@
+<footer class="main-footer">
+    <div class="footer-left">
+        Copyright &copy; <?php echo date("Y"); ?> <div class="bullet"></div>
+        Made By Peter
+    </div>
+    <div class="footer-right">
+        2.3.0
+    </div>
+</footer>
