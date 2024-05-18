d9063559cc2f5957a7b1ced5658bc3574040a2f1
ptrjs <peterjose64@gmail.com>
2024-05-18 23:14:18 +0700

    crud users, categories, products

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,9 +2,12 @@
 
 namespace Database\Seeders;
 
+use App\Models\Category;
+use App\Models\Product;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,11 +16,29 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        User::factory(10)->create();
 
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'name' => 'peter',
+            'email' => 'peter@wisata.com',
+            'password' => Hash::make('12345678'),
         ]);
+
+        Category::factory()->create([
+            'name'=>'DOMESTIK',
+            'description'=>'Tiket Domestik'
+        ]);
+
+        Category::factory()->create([
+            'name'=>'PARKIR',
+            'description'=>'Tiket Parkir'
+        ]);
+
+        Category::factory()->create([
+            'name'=>'CEMILAN',
+            'description'=>'Cemilan'
+        ]);
+
+        Product::factory(10)->create();
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,22 @@
 <?php
 
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('pages.dashboard');
+    return view('pages.auth.login');
 });
+
+Route::middleware(['auth'])->group(function(){
+    Route::get('/home', function(){
+        return view('pages.dashboard');
+    })->name('home');
+
+    Route::resource('users', UserController::class);
+    Route::resource('categories', CategoryController::class);
+    Route::resource('products', ProductController::class);
+});
+
+
